125c7f0fefb0be28d7a6c73fb3eef9886239bae9
Josh Ganote <j.ganote0825@gmail.com>
2020-01-31 14:05:53 -0600

    added process.env to all values inside new pool, and added user and password

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,11 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 const pool = new Pool ({
-    database: "koala_holla",
-    host: "localhost",
-    port: 5432,
+    database: process.env.POSTGRES_DB ? process.env.POSTGRES_DB : "koala_holla",
+    host: process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "localhost",
+    user: process.env.POSTGRES_USER ? process.env.POSTGRES_USER : 'postgres',
+    password: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : 'postgres',
+    port: process.env.PORT_DB ? process.env.PORT_DB : 5432,
     max: 10,
     idleTimeoutMillis: 30000
 });
